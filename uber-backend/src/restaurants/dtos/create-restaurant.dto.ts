b356c3a69f91fa8b2737f27abcdc2c25d127153b
hou27 <ataj125@gmail.com>
2021-09-30 11:06:17 +0000

    validate args

--- uber-backend/src/restaurants/dtos/create-restaurant.dto.ts
+++ uber-backend/src/restaurants/dtos/create-restaurant.dto.ts
@@ -1,13 +1,32 @@
 import { ArgsType, Field } from '@nestjs/graphql';
+import { IsBoolean, IsString, Length } from 'class-validator';
 
 @ArgsType()
 export class CreateRestaurantDto {
-	@Field((type) => String)
-	name: string;
-	@Field((type) => Boolean)
-	isVegan: boolean;
-	@Field((type) => String)
-	address: string;
-	@Field((type) => String)
-	ownersName: string;
+  @Field(type => String)
+  @IsString()
+  @Length(5, 10)
+  name: string;
+
+  @Field(type => Boolean)
+  @IsBoolean()
+  isVegan: boolean;
+
+  @Field(type => String)
+  @IsString()
+  address: string;
+
+  @Field(type => String)
+  @IsString()
+  ownersName: string;
 }
+/* on graphqlplayground works well
+mutation {
+  createRestaurant(
+    name: "1241254wre1221214",
+	isVegan: false,
+    address: "12509294",
+    ownersName: "HOu",
+  )
+}
+*/

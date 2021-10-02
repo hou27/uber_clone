f8614b209a864e644d5d56fcd472acf0c068c9a6
hou27 <ataj125@gmail.com>
2021-10-02 06:04:34 +0000

    second way to fix 'parent and child types are different err' (at entity)

--- uber-backend/src/restaurants/dtos/create-restaurant.dto.ts
+++ uber-backend/src/restaurants/dtos/create-restaurant.dto.ts
@@ -3,14 +3,4 @@ import { IsBoolean, IsString, Length } from 'class-validator';
 import { Restaurant } from '../entities/restaurant.entity';
 
 @InputType()
-export class CreateRestaurantDto extends OmitType(
-	Restaurant,
-	['id'],
-	InputType /* add an arg to fix err */
-) {} // omit id in restaurant
-
-/**
- * If the parent and child types are different,
- * (e.g., the parent is decorated with @ObjectType),
- * we would pass InputType as the second argument.
- */
+export class CreateRestaurantDto extends OmitType(Restaurant, ['id']) {} // omit id in restaurant
--- uber-backend/src/restaurants/entities/restaurant.entity.ts
+++ uber-backend/src/restaurants/entities/restaurant.entity.ts
@@ -1,30 +1,39 @@
-import { Field, ObjectType } from '@nestjs/graphql';
+import { Field, InputType, ObjectType } from '@nestjs/graphql';
+import { IsBoolean, IsString, Length } from 'class-validator';
 import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
-@ObjectType()	// ObjectType for graphql
-@Entity()	// Entity for typeorm
-export class Restaurant {	// Data Mapper pattern
-  @PrimaryGeneratedColumn()
-  @Field(type => Number)
-  id: number;
+// Schema must contain uniquely named types(Second way to fix err)
+@InputType({ isAbstract: true })	// isAbstract : do not add to Schema
+@ObjectType()
+@Entity()
+export class Restaurant {
+	@PrimaryGeneratedColumn()
+	@Field((type) => Number)
+	id: number;
 
-  @Field(type => String)
-  @Column()
-  name: string;
+	@Field((type) => String)
+	@Column()
+	@IsString()
+	@Length(5)
+	name: string;
 
-  @Field(type => Boolean)
-  @Column()
-  isVegan: boolean;
+	@Field((type) => Boolean)
+	@Column()
+	@IsBoolean()
+	isVegan: boolean;
 
-  @Field(type => String)
-  @Column()
-  address: string;
+	@Field((type) => String)
+	@Column()
+	@IsString()
+	address: string;
 
-  @Field(type => String)
-  @Column()
-  ownersName: string;
+	@Field((type) => String)
+	@Column()
+	@IsString()
+	ownersName: string;
 
-  @Field(type => String)
-  @Column()
-  categoryName: string;
+	@Field((type) => String)
+	@Column()
+	@IsString()
+	categoryName: string;
 }

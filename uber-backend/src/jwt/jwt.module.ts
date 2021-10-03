1d714984c19abe5fe4b6913c6b5f5bfcb65de20a
hou27 <ataj125@gmail.com>
2021-10-03 05:56:29 +0000

    prev commit was mistake. It is real working with modules without Global option.

--- uber-backend/src/jwt/jwt.module.ts
+++ uber-backend/src/jwt/jwt.module.ts
@@ -2,7 +2,6 @@ import { DynamicModule, Global, Module } from '@nestjs/common';
 import { JwtService } from './jwt.service';
 
 @Module({})
-@Global()
 export class JwtModule {
 	static forRoot(): DynamicModule {
 		return {
--- uber-backend/src/users/users.module.ts
+++ uber-backend/src/users/users.module.ts
@@ -3,10 +3,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
 import { UserResolver } from './users.resolver';
 import { UserService } from './users.service';
-import { JwtService } from 'src/jwt/jwt.service';
+import { JwtModule } from 'src/jwt/jwt.module';
 
 @Module({
-	imports: [TypeOrmModule.forFeature([User]), JwtService],
+	imports: [TypeOrmModule.forFeature([User]), JwtModule.forRoot()],
 	providers: [UserResolver, UserService],
 })
 export class UsersModule {}

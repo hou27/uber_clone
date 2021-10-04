83c1ae7017286c31d5f62ce12d6de88fa94f7278
hou27 <ataj125@gmail.com>
2021-10-04 08:04:43 +0000

    mutation for userProfile

--- uber-backend/src/users/dtos/create-account.dto.ts
+++ uber-backend/src/users/dtos/create-account.dto.ts
@@ -1,9 +1,9 @@
 import { Field, InputType, ObjectType, PickType } from '@nestjs/graphql';
 import { User } from '../entities/user.entity';
-import { MutationOutput } from 'src/common/dtos/output.dto';
+import { CoreOutput } from 'src/common/dtos/output.dto';
 
 @InputType()
 export class CreateAccountInput extends PickType(User, ['email', 'password', 'role']) {}
 
 @ObjectType()
-export class CreateAccountOutput extends MutationOutput {}
+export class CreateAccountOutput extends CoreOutput {}
--- uber-backend/src/users/users.resolver.ts
+++ uber-backend/src/users/users.resolver.ts
@@ -6,6 +6,7 @@ import { LoginInput, LoginOutput } from './dtos/login.dto';
 import { UseGuards } from '@nestjs/common';
 import { AuthGuard } from 'src/auth/auth.guard';
 import { AuthUser } from 'src/auth/auth-user.decorator';
+import { UserProfileInput, UserProfileOutput } from './dtos/user-profile.dto';
 
 @Resolver((of) => User)
 export class UserResolver {
@@ -56,5 +57,25 @@ export class UserResolver {
 	me(@AuthUser() authUser: User) {
 		// decorator has to return value
 		return authUser;
+	}
+
+	@UseGuards(AuthGuard)
+	@Query((returns) => UserProfileOutput)
+	async userProfile(@Args() userProfileInput: UserProfileInput): Promise<UserProfileOutput> {
+		try {
+			const user = await this.usersService.findById(userProfileInput.userId);
+			if (!user) {
+				throw Error();
+			}
+			return {
+				ok: true,
+				user,
+			};
+		} catch (e) {
+			return {
+				error: 'User Not Found',
+				ok: false,
+			};
+		}
 	}
 }

e18f677b7123fb55f058deb24f7a41cac0bec849
hou27 <ataj125@gmail.com>
2021-10-11 05:18:31 +0000

    Mocking for Unit Test

--- uber-backend/src/users/users.service.spec.ts
+++ uber-backend/src/users/users.service.spec.ts
@@ -1,12 +1,51 @@
 import { Test } from '@nestjs/testing';
 import { UserService } from './users.service';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { JwtService } from 'src/jwt/jwt.service';
+import { MailService } from 'src/mail/mail.service';
+import { User } from './entities/user.entity';
+import { Verification } from './entities/verification.entity';
+
+
+const mockRepository = {	// fake for Unit Test
+	findOne: jest.fn(), // create Mock Func
+	save: jest.fn(),
+	create: jest.fn(),
+};
+
+const mockJwtService = {
+	sign: jest.fn(),
+	verify: jest.fn(),
+};
+
+const mockMailService = {
+	sendVerificationEmail: jest.fn(),
+};
 
 describe('UserService', () => {
 	let service: UserService;
 
 	beforeAll(async () => {
 		const module = await Test.createTestingModule({
-			providers: [UserService],
+			providers: [
+				UserService,
+				{
+					provide: getRepositoryToken(User),
+					useValue: mockRepository,
+				},
+				{
+					provide: getRepositoryToken(Verification),
+					useValue: mockRepository,
+				},
+				{
+					provide: JwtService,
+					useValue: mockJwtService,
+				},
+				{
+					provide: MailService,
+					useValue: mockMailService,
+				},
+			],
 		}).compile();
 		service = module.get<UserService>(UserService);
 	});

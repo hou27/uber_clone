d3ee0fea76fbc07a243a4ca0d6a0b7ec57a9d057
hou27 <ataj125@gmail.com>
2022-01-13 16:11:00 +0900

    Remove rootQuery

--- uber-backend/src/users/users.resolver.ts
+++ uber-backend/src/users/users.resolver.ts
@@ -1,7 +1,10 @@
 import { Resolver, Query, Mutation, Args, Context } from '@nestjs/graphql';
 import { User } from './entities/user.entity';
 import { UserService } from './users.service';
-import { CreateAccountInput, CreateAccountOutput } from './dtos/create-account.dto';
+import {
+  CreateAccountInput,
+  CreateAccountOutput,
+} from './dtos/create-account.dto';
 import { LoginInput, LoginOutput } from './dtos/login.dto';
 import { UseGuards } from '@nestjs/common';
 import { AuthGuard } from 'src/auth/auth.guard';
@@ -13,50 +16,48 @@ import { VerifyEmailInput, VerifyEmailOutput } from './dtos/verify-email.dto';
 
 @Resolver((of) => User)
 export class UserResolver {
-	constructor(private readonly usersService: UserService) {}
+  constructor(private readonly usersService: UserService) {}
 
-	@Query((returns) => Boolean)
-	rootQuery() {
-		return true;
-	}
-	
-	@Mutation((returns) => CreateAccountOutput)
-	async createAccount(
-		@Args('input') createAccountInput: CreateAccountInput
-	): Promise<CreateAccountOutput> {
-		return this.usersService.createAccount(createAccountInput);
-	}
+  @Mutation((returns) => CreateAccountOutput)
+  async createAccount(
+    @Args('input') createAccountInput: CreateAccountInput,
+  ): Promise<CreateAccountOutput> {
+    return this.usersService.createAccount(createAccountInput);
+  }
 
-	@Mutation((returns) => LoginOutput)
-	async login(@Args('input') loginInput: LoginInput): Promise<LoginOutput> {
-		return this.usersService.login(loginInput);
-	}
+  @Mutation((returns) => LoginOutput)
+  async login(@Args('input') loginInput: LoginInput): Promise<LoginOutput> {
+    return this.usersService.login(loginInput);
+  }
 
-	@Query((returns) => User)
-	@Role(["Any"]) // set metadata
-	me(@AuthUser() authUser: User) {
-		// decorator has to return value
-		return authUser;
-	}
+  @Query((returns) => User)
+  @Role(['Any']) // set metadata
+  me(@AuthUser() authUser: User) {
+    // decorator has to return value
+    return authUser;
+  }
 
-	@Query((returns) => UserProfileOutput)
-	@Role(["Any"])
-	async userProfile(@Args() userProfileInput: UserProfileInput): Promise<UserProfileOutput> {
-		return this.usersService.findById(userProfileInput.userId);
-	}
+  @Query((returns) => UserProfileOutput)
+  @Role(['Any'])
+  async userProfile(
+    @Args() userProfileInput: UserProfileInput,
+  ): Promise<UserProfileOutput> {
+    return this.usersService.findById(userProfileInput.userId);
+  }
 
-	
-	@Mutation((returns) => EditProfileOutput)
-	@Role(['Any'])
-	async editProfile(
-		@AuthUser() authUser: User,
-		@Args('input') editProfileInput: EditProfileInput
-	): Promise<EditProfileOutput> {
-		return this.usersService.editProfile(authUser.id, editProfileInput);
-	}
+  @Mutation((returns) => EditProfileOutput)
+  @Role(['Any'])
+  async editProfile(
+    @AuthUser() authUser: User,
+    @Args('input') editProfileInput: EditProfileInput,
+  ): Promise<EditProfileOutput> {
+    return this.usersService.editProfile(authUser.id, editProfileInput);
+  }
 
-	@Mutation((returns) => VerifyEmailOutput)
-	verifyEmail(@Args('input') { code }: VerifyEmailInput): Promise<VerifyEmailOutput> {
-		return this.usersService.verifyEmail(code);
-	}
+  @Mutation((returns) => VerifyEmailOutput)
+  verifyEmail(
+    @Args('input') { code }: VerifyEmailInput,
+  ): Promise<VerifyEmailOutput> {
+    return this.usersService.verifyEmail(code);
+  }
 }
